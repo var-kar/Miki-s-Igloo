c594ce126e1685b288b71307a97df0e23b431ee4
Karthik Vasudevan <karthik@wearepercent.com>
2017-08-19 00:30:02 +0100

    first commit

---
+++ Miki's Igloo/Factories/Background.swift
@@ -0,0 +1,9 @@
+//
+//  Background.swift
+//  Miki's Igloo
+//
+//  Created by Karthik Vasudevan on 17/08/2017.
+//  Copyright © 2017 Karthik Vasudevan. All rights reserved.
+//
+
+import Foundation
---
+++ Miki's Igloo/Factories/Miki.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Miki's Igloo
+//
+//  Created by Karthik Vasudevan on 18/08/2017.
+//  Copyright © 2017 Karthik Vasudevan. All rights reserved.
+//
+
+import Foundation
